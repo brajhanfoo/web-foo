1a2ea341a41e3f21ba80857e19d21369258a9b78
brajhanfoo <brajhan.ceo@footalentgroup.com>
2025-08-20 01:57:24 -0500

    feat:add logo main principal

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,5 +1,12 @@
+import Image from 'next/image'
+import { LOGO } from '@/lib/imagePaths'
+
 const page = () => {
-  return <div>services</div>
+  return (
+    <main className="flex flex-col items-center justify-center h-screen bg-gray-900">
+      <Image src={LOGO} alt="Logo de FooTalentGroup" width={300} height={300} />
+    </main>
+  )
 }
 
 export default page
---
+++ src/lib/imagePaths.ts
@@ -0,0 +1,2 @@
+export const LOGO =
+  'https://res.cloudinary.com/dtaybaydq/image/upload/fl_preserve_transparency/v1743213874/Foo%20Talent%20group/logo.jpg?_s=public-apps'
