4b712c81195da491f13abaf6161110479c5b500f
brajhanfoo <brajhan.ceo@footalentgroup.com>
2026-01-26 16:13:29 -0500

    fix: web mvp

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -9,12 +9,22 @@ import ProjectsSection from './components/ProjectsSection'
 
 const page = () => {
   return (
-    <main className=" bg-black">
-      <Hero />
-      <ServicesSection services={services} />
-      <ProjectsSection />
-      <TestimoniosSection />
-      <FaqSection />
+    // <main className=" bg-black">
+    //   {/* <Hero />
+    //   <ServicesSection services={services} />
+    //   <ProjectsSection />
+    //   <TestimoniosSection />
+    //   <FaqSection /> */}
+    // </main>
+        <main className="min-h-screen flex flex-col items-center justify-center bg-black text-white text-center px-6">
+      <h1 className="text-4xl md:text-5xl font-bold mb-4">
+        Página en construcción
+      </h1>
+      <p className="text-lg md:text-xl text-gray-300 max-w-xl">
+        Estamos trabajando para brindarte una mejor experiencia.
+        <br />
+        Muy pronto tendrás novedades.
+      </p>
     </main>
   )
 }
